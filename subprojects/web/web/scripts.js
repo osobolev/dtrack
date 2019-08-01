1b6a68117f9ff344261797cc780bfc065146e2f4
oleg <sobolev_ov@mail.ru>
2019-08-01 19:06:04 +0300

    Сохранение/восстановление сортировки

--- subprojects/web/web/scripts.js
+++ subprojects/web/web/scripts.js
@@ -28,6 +28,42 @@ function configTable(elem, pageSize, what, columns) {
         "iDisplayLength": pageSize,
         "order": [],
         "autoWidth": false,
+        "stateSave": true,
+        "stateSaveCallback": function (settings, data) {
+            var order = data['order'];
+            if (!order)
+                return;
+            var query = '';
+            for (var i = 0; i < order.length; i++) {
+                var col = order[i];
+                var colIndex = col[0];
+                var dir = col[1];
+                query += '&' + colIndex + '=' + dir;
+            }
+            if (query.length > 0) {
+                query = query.substring(1);
+            }
+            window.location.hash = '#' + query;
+        },
+        "stateLoadCallback": function (settings, callback) {
+            var hash = window.location.hash;
+            var orders = [];
+            if (hash && hash.length > 1) {
+                var parts = hash.substring(1).split('&');
+                for (var i = 0; i < parts.length; i++) {
+                    var part = parts[i].split('=');
+                    if (part.length < 2)
+                        continue;
+                    var colIndex = parseInt(part[0]);
+                    var order = part[1];
+                    orders.push([colIndex, order]);
+                }
+            }
+            return {
+                "time": new Date().getTime(),
+                "order": orders
+            };
+        },
         "columns": columns
     });
 }
